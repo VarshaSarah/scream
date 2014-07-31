6033f287271239c5097b2c03bdd2976f24ebca93
basnal <pankajbasnal17@gmail.com>
2014-07-31 12:37:07 +0530

    Mongoid config file generated #4

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,16 @@
+require 'sidekiq/web'
+
 Scream::Engine.routes.draw do
+  resources :iframe_contents
+  resources :contents do
+    member do
+      get 'details'
+    end
+    collection do
+      get 'tags'
+    end
+  end
+
+  root 'contents#index'
+  mount Sidekiq::Web, at:'/sidekiq'
 end
--- scream.gemspec
+++ scream.gemspec
@@ -20,4 +20,6 @@ Gem::Specification.new do |s|
   s.add_dependency "rails", "~> 4.1.1"
   s.add_development_dependency 'rspec-rails'
   s.add_development_dependency 'factory_girl_rails'
+  s.add_development_dependency 'sidekiq'
+  s.add_development_dependency 'mongoid'
 end
