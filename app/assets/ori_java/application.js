dc4e18c734e4903bf92d1589aa93c9227f6ae822
basnal <pankajbasnal17@gmail.com>
2014-07-31 13:29:22 +0530

    Stylesheets added #5

--- app/assets/ori_java/application.js
+++ app/assets/ori_java/application.js
@@ -10,24 +10,4 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require scream/jquery.tokeninput
-
-//= require bootstrap/affix
-//= require bootstrap/alert
-//= require bootstrap/button
-//= require bootstrap/carousel
-//= require bootstrap/collapse
-//= require bootstrap/dropdown
-//= require bootstrap/tab
-//= require bootstrap/transition
-//= require bootstrap/scrollspy
-//= require bootstrap/modal
-//= require bootstrap/tooltip
-//= require bootstrap/popover
-//= require scream/filter
-//= require scream/mustache
-//= require scream/global
-//= require contents
+//= require_tree .
--- app/controllers/scream/application_controller.rb
+++ app/controllers/scream/application_controller.rb
@@ -1,4 +1,12 @@
 module Scream
   class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    before_action :check_headers
+
+    private
+
+    def check_headers
+      response.headers.delete("X-Frame-Options")
+    end
   end
 end
